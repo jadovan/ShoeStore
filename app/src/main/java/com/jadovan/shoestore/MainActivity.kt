d20cc16ba2983104d9da6ea0e22d9e02c463066c
jadovan <jadovan@hotmail.com>
2021-01-03 11:44:19 -0500

    Updated to include DataBindingUtil

--- app/src/main/java/com/jadovan/shoestore/MainActivity.kt
+++ app/src/main/java/com/jadovan/shoestore/MainActivity.kt
@@ -2,32 +2,49 @@ package com.jadovan.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.jadovan.shoestore.databinding.ActivityMainBinding
+import com.jadovan.shoestore.databinding.ShoeListFragmentBinding
 import com.jadovan.shoestore.models.login.LoginFragmentDirections
+import com.jadovan.shoestore.models.shoe_list.ShoeListFragmentDirections
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        ).apply {
+            viewBinding = this
+            setSupportActionBar(this.toolbar)
+            appBarConfiguration = AppBarConfiguration(navController.graph)
+            setupActionBarWithNavController(
+                navController,
+                appBarConfiguration
+            )
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp()
     }
 
-    override fun onBackPressed() {
-        //val navController = findNavController(R.id.nav_host_fragment)
-        //return navController.navigate(LoginFragmentDirections.actionLoginToWelcome())
-    }
+    override fun onBackPressed() {}
+
 }
