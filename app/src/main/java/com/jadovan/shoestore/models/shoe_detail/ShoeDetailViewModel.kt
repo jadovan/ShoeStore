d4151e25aef6dfb99bf28c62c663db398e964b35
jadovan <jadovan@hotmail.com>
2021-01-03 11:59:28 -0500

    Updated OnSave function to display message when handling events

--- app/src/main/java/com/jadovan/shoestore/models/shoe_detail/ShoeDetailViewModel.kt
+++ app/src/main/java/com/jadovan/shoestore/models/shoe_detail/ShoeDetailViewModel.kt
@@ -1,10 +1,12 @@
 package com.jadovan.shoestore.models.shoe_detail
 
+import com.jadovan.shoestore.Event
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
+import com.jadovan.shoestore.R
 
 class ShoeDetailViewModel : ViewModel() {
 
@@ -17,14 +19,19 @@ class ShoeDetailViewModel : ViewModel() {
         _shoeDetails.value = ArrayList()
     }
 
+    // Construct object for Toast message
+    private val _message = MutableLiveData<Event<Int>>()
+    val message: LiveData<Event<Int>>
+        get() = _message
+
     // Function for saving the shoe details
     fun onSave(view: View, shoe: Shoe) =
         if (shoe.name.isNotEmpty() && shoe.company.isNotEmpty() && shoe.size >= 0.0) {
             _shoeDetails.value?.add(shoe)
+            _message.value = Event(R.string.shoe_detail_success)
             onNavigateUp(view)
-        }
-        else {
-            // Attempted to use Toast, but creating warning note in details screen
+        } else {
+            _message.value = Event(R.string.shoe_detail_warning)
         }
 
     // Function for the navigation of the up button
